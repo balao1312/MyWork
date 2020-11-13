85f6166f1d12e3cc18787bdc66eedb5a3cb012bc
balao1312 <balao1014@hotmail.com>
2020-11-13 09:50:15 +0800

    clean up

--- system_monitoring/start.sh
+++
@@ -1,4 +0,0 @@
-python3 -u check_server.py >/dev/null 2>>errorlog &
-python3 -u logging_usage.py >/dev/null 2>>errorlog &
-python3 -u get_server_status_remote.py >/dev/null 2>>errorlog &
-python3 -u keep_heroku_awake.py >/dev/null 2>>errorlog &
